8c4c091a12c5cc68efeaa7301c89181b70492d0e
LeeJongAnn <dlwhddksiq@naver.com>
2023-10-16 20:17:26 +0900

    엔티티 클래스 작성 1 - FK가 있는 곳을 연관관계 주인으로 삼는다 - Order에 delivery가 존재하고 Delivery에 Order가 존재한다 그렇다는 것은 하나는 연관관계의 주인으로 삼고 , 다른 하나는 mappedby로 묶어줘야 한다.

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain;
 
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,11 +16,13 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
-    private Item item;
-
 
     @ManyToOne
-    @JoinColumn
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice;
