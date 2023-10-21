0201229a867d6fa8747d6ed23f9dae60cabd22f2
LeeJongAnn <dlwhddksiq@naver.com>
2023-10-21 19:50:53 +0900

    회원 레포지토리 작성 및 회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop/domain/service/MemberService.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    //회원 가입
+    public Long join(Member member){
+
+        validateDuplicatedMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicatedMember(Member member) {
+        List<Member> findMember = memberRepository.findByName(member.getName());
+        if(!findMember.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long MemberId) {
+        return memberRepository.findOne(MemberId);
+    }
+
+}
